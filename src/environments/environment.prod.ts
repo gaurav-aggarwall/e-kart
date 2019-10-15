096ef81cd35271c908a662007c493f0f225cde83
Gaurav Aggarwal <aggarwal.gaurav10@gmail.com>
2019-10-15 16:37:49 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBh6-dT8TVhigzBTPzJGCTQEsTfRAgb0vQ",
+    authDomain: "ekart-app.firebaseapp.com",
+    databaseURL: "https://ekart-app.firebaseio.com",
+    projectId: "ekart-app",
+    storageBucket: "ekart-app.appspot.com",
+    messagingSenderId: "381077128058",
+    appId: "1:381077128058:web:37055e4c6569a822daccc3",
+    measurementId: "G-LT51YHB9XM"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBh6-dT8TVhigzBTPzJGCTQEsTfRAgb0vQ",
+    authDomain: "ekart-app.firebaseapp.com",
+    databaseURL: "https://ekart-app.firebaseio.com",
+    projectId: "ekart-app",
+    storageBucket: "ekart-app.appspot.com",
+    messagingSenderId: "381077128058",
+    appId: "1:381077128058:web:37055e4c6569a822daccc3",
+    measurementId: "G-LT51YHB9XM"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
