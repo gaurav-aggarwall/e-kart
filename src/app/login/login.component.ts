9d3ae9f7aaab00b21b9e38e8b7b2c41fbe1d7bd3
Gaurav Aggarwal <aggarwal.gaurav10@gmail.com>
2019-10-16 14:16:51 +0530

    Google Authentication Added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+	constructor(private afAuth: AngularFireAuth) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	login() {
+		this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+	}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+	user$: Observable<firebase.User>;
 
-  constructor() { }
+	constructor(private afAuth: AngularFireAuth) {
+		this.user$ = afAuth.authState;
+	}
 
-  ngOnInit() {
-  }
+	logout() {
+		this.afAuth.auth.signOut();
+	}
 
 }
